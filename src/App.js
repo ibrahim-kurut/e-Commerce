052553336f0ec3bce82698c2789f20c517bd23ad
ibrahim kurut <102996120+ibrahim-kurut@users.noreply.github.com>
2024-05-14 18:06:23 +0300

    Add tailwindcss to the project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>eCommerce</h1>
+      <h1 className="text-3xl  font-bold underline">
+        eCommerce
+      </h1>
     </div>
   );
 }
